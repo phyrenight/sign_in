4a3e360a301baf2c800b943b63ae5be6c3a9012f
Marc <phyrenight@gmail.com>
2020-09-05 14:29:43 -0400

    add modal functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./style.css";
 import "./normalize.css";
+
+window.onload = function(){
+    const button = document.getElementById("log_in");
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const section_modal = document.getElementById("section_modal");
+    const section_sign_in = document.getElementById("section_sign_in");
+    button.onclick = function(){
+        console.log(window)
+        if((username.checkValidity() && password.checkValidity()) == true){
+            section_modal.style.display = "block";
+            section_sign_in.style.display = "none";
+        }else {
+            console.log("All fields must be filled in.")
+        }
+    }
+}
